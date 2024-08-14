c3ba51325e6cb73228812d00ece9322e6b7fab53
Arhaan Sayeed <dev.sayarhaan@gmail.com>
2024-08-14 18:58:02 +0530

    projects commit

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import SectionHeading from './section-heading'
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Image from "next/image";
+
+type projectProps = (typeof projectsData)[number]
 
 export default function Projects() {
   return (
     <section>
-        <SectionHeading>Projects</SectionHeading>
-      
+      <SectionHeading>Projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
-  )
+  );
+}
+
+function Project({ title, description, tags, imageUrl }: projectProps) {
+  return (
+    <section>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>{tags.map(((tag, index) => (
+        <li key={index}>{tag}</li>
+      )))}</ul>
+    </section>
+  );
 }
